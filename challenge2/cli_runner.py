e493c24b9f11f2fd18b0383352db1031986ecdd4
redraincatching <99604494+redraincatching@users.noreply.github.com>
2023-03-07 13:45:36 +0000

    challenge 2 dded

---
+++ challenge2/cli_runner.py
@@ -0,0 +1,9 @@
+from challenge2part1 import problem2_1, problem2_2, problem2_3
+from challenge2part2 import problem2_4, problem2_5, problem2_6
+
+print('Bot response:', problem2_1(''))
+print('Bot response:', problem2_2(''))
+print('Bot response:', problem2_3(''))
+print('Bot response:', problem2_4(''))
+print('Bot response:', problem2_5(''))
+print('Bot response:', problem2_6(''))
